e8d922539b88c732244395dcf0def9d0cdc867b3
andrey-maslov <andrei.maslau@gmail.com>
2020-01-08 15:08:04 +0300

    some changes with modal render

--- src/components/Music/Albums/AlbumView/TrackItem/TrackItem.js
+++ src/components/Music/Albums/AlbumView/TrackItem/TrackItem.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Modal from 'react-modal';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import { faFile } from '@fortawesome/free-regular-svg-icons';
+import {faFile} from '@fortawesome/free-regular-svg-icons';
 
 
 import styles from './TrackItem.module.css'
@@ -10,7 +10,7 @@ Modal.setAppElement('#root');
 
 export default class TrackItem extends React.Component {
 
-    constructor(props){
+    constructor(props) {
         super(props);
 
         this.state = {
@@ -24,7 +24,8 @@ export default class TrackItem extends React.Component {
     openModal() {
         this.setState({
             modalIsOpen: true
-        })
+        });
+        document.body.classList.add('modal-opened');
     }
 
     afterOpenModal() {
@@ -37,27 +38,39 @@ export default class TrackItem extends React.Component {
         })
     }
 
-    render(){
+    render() {
+
+        const modal = this.state.modalIsOpen
+            ? <Modal
+                className={styles.modal}
+                isOpen={this.state.modalIsOpen}
+                onAfterOpen={this.afterOpenModal}
+                onRequestClose={this.closeModal}
+                shouldCloseOnOverlayClick={true}
+                contentLabel="Example Modal"
+            >
+                <button className={styles.closeBtn} onClick={this.closeModal}>kill</button>
+                <div className={styles.modalContent}>
+                    <h4
+                        className={styles.modalTitle}
+                        dangerouslySetInnerHTML={{__html: this.props.track.song}}
+                    />
+                    <div
+                        className={styles.modalBody}
+                        dangerouslySetInnerHTML={{__html: this.props.track.lyrics}}
+                    />
+                </div>
+            </Modal>
+            : null
+
         return (
-            <li className={styles.track}>
-                <h5 className={styles.trackTitle}>{this.props.track.song}</h5>
-                <button className={styles.openBtn} onClick={this.openModal}>
-                    <FontAwesomeIcon icon={faFile} />
-                </button>
-                <Modal
-                    className={styles.modal}
-                    isOpen={this.state.modalIsOpen}
-                    onAfterOpen={this.afterOpenModal}
-                    onRequestClose={this.closeModal}
-                    shouldCloseOnOverlayClick={true}
-                    contentLabel="Example Modal"
-                >
-                    <button className={styles.closeBtn} onClick={this.closeModal}>close</button>
-                    <div className={styles.modalContent}>
-                        <h4 dangerouslySetInnerHTML={{__html: this.props.track.song}}/>
-                        <div dangerouslySetInnerHTML={{__html: this.props.track.lyrics}}/>
-                    </div>
-                </Modal>
+            <li className={styles.item}>
+                <div className={styles.track}>
+                    <h5 className={styles.trackTitle}>{this.props.track.song}</h5>
+                    <button className={styles.openBtn} onClick={this.openModal}>
+                        <FontAwesomeIcon icon={faFile}/>
+                    </button>
+                    {modal}</div>
             </li>
         )
     }
